93f9ea5bea8aa75202a5cae3fec93463eb813e48
Adam Bergman <adam.bergman@bluediesel.com>
2013-07-25 10:39:00 -0400

    Added empty Xcode project

---
+++ iRepEmulator/iRepEmulator/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  iRepEmulator
+//
+//  Created by Bergman, Adam on 7/25/13.
+//  Copyright (c) 2013 Adam Bergman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iRepEmulator/iRepEmulator/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  iRepEmulator
+//
+//  Created by Bergman, Adam on 7/25/13.
+//  Copyright (c) 2013 Adam Bergman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
